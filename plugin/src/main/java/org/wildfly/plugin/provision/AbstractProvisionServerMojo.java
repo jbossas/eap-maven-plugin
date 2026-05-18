833d3be61ab2ee16c380f2bce6e6bb71c6ac2fef
Jean-Francois Denise <jfdenise@gmail.com>
2026-05-18 10:43:23 +0200

    [CONVERSION COMMIT] Remove un-used import

--- plugin/src/main/java/org/wildfly/plugin/provision/AbstractProvisionServerMojo.java
+++ plugin/src/main/java/org/wildfly/plugin/provision/AbstractProvisionServerMojo.java
@@ -13,7 +13,6 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
