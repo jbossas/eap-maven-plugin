e7e96446e4061b554e233c050ac234228e5925b2
Jean-Francois Denise <jfdenise@gmail.com>
2026-06-05 14:24:15 +0200

    Fix for JBEAP-33249, eap-maven-plugin creates bootable JAR with wrong name (server--bootable.jar instead of {artifactId}-bootable.jar)

--- plugin/src/main/java/org/wildfly/plugin/provision/PackageServerMojo.java
+++ plugin/src/main/java/org/wildfly/plugin/provision/PackageServerMojo.java
@@ -250,8 +250,7 @@ public class PackageServerMojo extends AbstractProvisionServerMojo {
      *
      * @since 5.0
      */
-    @Parameter(alias = "bootable-jar-name", property = PropertyNames.BOOTABLE_JAR_NAME, defaultValue = BOOTABLE_JAR_NAME_RADICAL
-            + "bootable.jar")
+    @Parameter(alias = "bootable-jar-name", property = PropertyNames.BOOTABLE_JAR_NAME, defaultValue = "${project.artifactId}-bootable.jar")
     private String bootableJarName;
 
     /**
